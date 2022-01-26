160fa9fe39d7b0fd37945d309e45f1fd6a926dad
NaGiBaToR9000 <wpusti@yandex.ru>
2022-01-26 10:00:06 +0300

    second task is ready

--- GBhomework/Program.cs
+++ GBhomework/Program.cs
@@ -10,6 +10,54 @@ namespace GBhomework
     {
         static void Main(string[] args)
         {
-        }
+            //Запросить у пользователя порядковый номер текущего месяца и вывести его название.
+            Console.WriteLine("Введите порядковый номер месяца");
+            byte number = byte.Parse(Console.ReadLine());
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Январь");
+                    break;
+                case 2:
+                    Console.WriteLine("Февраль");
+                    break;
+                case 3:
+                    Console.WriteLine("Март");
+                    break;
+                case 4:
+                    Console.WriteLine("Апрель");
+                    break;
+                case 5:
+                    Console.WriteLine("Май");
+                    break;
+                case 6:
+                    Console.WriteLine("Июнь");
+                    break;
+                case 7:
+                    Console.WriteLine("Июль");
+                    break;
+                case 8:
+                    Console.WriteLine("Август");
+                    break;
+                case 9:
+                    Console.WriteLine("Сентябрь");
+                    break;
+                case 10:
+                    Console.WriteLine("Октябрь");
+                    break;
+                case 11:
+                    Console.WriteLine("Ноябрь");
+                    break;
+                case 12:
+                    Console.WriteLine("Декабрь");
+                    break;
+                default:
+                    Console.WriteLine("Месяца с таким порядковым номером нет :(");
+                    break;
+
+            }
+            Console.ReadKey();
+           
     }
+}
 }
