868d8c9d0dc928da90cc19b11e83f1989462bdc7
NaGiBaToR9000 <wpusti@yandex.ru>
2022-01-26 08:59:26 +0300

    Добавьте файлы проекта.

---
+++ GBhomework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBhomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
