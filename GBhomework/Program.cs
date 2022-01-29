eea070dbae474609886623d2e67c2031ccf4e67f
NaGiBaToR9000 <wpusti@yandex.ru>
2022-01-29 17:33:56 +0300

    third lesson first task

--- GBhomework/Program.cs
+++ GBhomework/Program.cs
@@ -10,6 +10,18 @@ namespace GBhomework
     {
         static void Main(string[] args)
         {
+            //Написать программу, выводящую элементы двухмерного массива по диагонали.
+
+            string[,] arr = { {"Написать", "программу", "выводящую", "элементы"}, {"двухмерного", "массива", "по", "диагонали"} };
+            string indention = "";
+            for (int i = 0; i < arr.GetLength(0); i++) {
+                for(int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.WriteLine($"{indention}{arr[i, j]}");
+                    indention += "      ";
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
