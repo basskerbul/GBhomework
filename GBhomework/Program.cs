5aaa9ad048c76ff47fa779561f802efbea78eb46
NaGiBaToR9000 <wpusti@yandex.ru>
2022-02-01 14:09:35 +0300

    first lesson first task

--- GBhomework/Program.cs
+++ GBhomework/Program.cs
@@ -8,8 +8,25 @@ namespace GBhomework
 {
     class Program
     {
+        static int Fibonacci(int num)
+        {
+            //0 1 1 и т.д
+            int[] fibnum = new int[num];
+            fibnum[0] = 0;
+            fibnum[1] = 1;
+            for(int i = 2; i < fibnum.Length; i++)
+            {
+                fibnum[i] = fibnum[i - 1] + fibnum[i - 2];
+            }
+            return fibnum[(num-1)];
+        }
         static void Main(string[] args)
         {
+            //(*) Написать программу, вычисляющую число Фибоначчи для заданного значения рекурсивным способом.
+
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fibonacci(number));
+            Console.ReadKey();
         }
     }
 }
