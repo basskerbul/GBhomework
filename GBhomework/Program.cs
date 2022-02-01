ef00c2ae5a06c365d26d70453f5c446e99920a8e
NaGiBaToR9000 <wpusti@yandex.ru>
2022-02-01 12:18:12 +0300

    fourth lesson first task

--- GBhomework/Program.cs
+++ GBhomework/Program.cs
@@ -8,8 +8,21 @@ namespace GBhomework
 {
     class Program
     {
+        static string GetFuiiName(string firstName, string lastName, string patronymic)
+        {
+            string fullName = $"{lastName} {firstName} {patronymic}";
+            return fullName;
+        }
         static void Main(string[] args)
         {
+            //Написать метод GetFullName(string firstName, string lastName, string patronymic), 
+            //принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО.
+            //Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
+            Console.WriteLine(GetFuiiName("Мириам", "Петросян", "Сергеевна"));
+            Console.WriteLine(GetFuiiName("Владимир", "Лебедев", "Иванович"));
+            Console.WriteLine(GetFuiiName("Михаил", "Булгаков", "Афанасьевич"));
+
+            Console.ReadKey();
         }
     }
 }
