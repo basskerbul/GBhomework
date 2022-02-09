8a0a5e22df34004c57759a2c8503b0afd0bbe860
NaGiBaToR9000 <wpusti@yandex.ru>
2022-02-09 21:56:40 +0300

    fifth lesson first task

--- GBhomework/Program.cs
+++ GBhomework/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace GBhomework
 {
@@ -10,6 +11,15 @@ namespace GBhomework
     {
         static void Main(string[] args)
         {
+            //Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+            string path = @"C:\Users\REFRESH\Desktop\1.txt";
+            string userText = Console.ReadLine();
+            if(File.Exists(path) == false)
+            {
+                File.Create(path);
+            }
+            File.WriteAllText(path, userText);
+            Console.ReadKey();
         }
     }
 }
