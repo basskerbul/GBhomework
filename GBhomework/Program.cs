2307f8bad41b377a749051a86ded757b61b93d97
NaGiBaToR9000 <wpusti@yandex.ru>
2022-02-13 11:13:01 +0300

    fifth lesson fourth task ask

--- GBhomework/Program.cs
+++ GBhomework/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace GBhomework
 {
-    class Program
-    {
+    class Program { 
+    
         static void Main(string[] args)
         {
+            //(*) Сохранить дерево каталогов и файлов по заданному пути в текстовый файл — с рекурсией и без.
+
+            string path = @"C:\";
+            string PathForTextFile = @"C:\Users\REFRESH\Desktop\DirectoryTree.txt";
+
+            string[] Dirs = Directory.GetDirectories(path);
+            string[] Files = Directory.GetFiles(path);
+
+            File.AppendAllText(PathForTextFile, $"Папки по пути {path}\n");
+            File.AppendAllLines(PathForTextFile, Dirs);
+
+            for(int i = 0; i < Dirs.Length; i++)
+            {
+                DirectoryInfo newDir = new DirectoryInfo(Dirs[i]);
+                path = newDir.FullName;
+                string[] SubDirs = Directory.GetDirectories(path);
+                string[] SubFiles = Directory.GetFiles(path);
+                File.AppendAllLines(PathForTextFile, SubFiles);
+                File.AppendAllLines(PathForTextFile, SubDirs);
+            }
+
+            File.AppendAllText(PathForTextFile, "Файлы:\n");
+            File.AppendAllLines(PathForTextFile, Files);
         }
     }
 }
